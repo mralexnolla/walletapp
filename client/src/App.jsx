b4498fcc86bda812cb51b92f16aea6624209d4b4
alex <nba4everwanted@gmail.com>
2023-07-13 07:08:23 +0000

    Revert "worked on the routing and added some styles in the in the alignment and the also added google fonts"
    This reverts commit edfe78e3e76c41556afc75dbb008444f8918844b.
    
    uncommiting to revert the changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,15 @@
-import Login from "./pages/login/Login"
-import Register from "./pages/register/Register"
-import "./stylesheets/alignments.css"
-import "./stylesheets/custome-components.css"
-import "./stylesheets/form-elements.css"
 import "./stylesheets/text-elements.css"
+import "./stylesheets/form-elements.css"
+import "./stylesheets/custome-components.css"
+import "./stylesheets/alignments.css"
 import "./stylesheets/theme.css"
-import {Routes, Route} from "react-router-dom"
 
 function App() {
  
 
   return (
     <div>
-     <Routes>
-       <Route path="/login" element={<Login />}/>
-       <Route path="/register" element={<Register />}/>
-     </Routes>
-      
+      Wallet App
     </div>
   );
 }
