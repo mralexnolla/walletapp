08e748b4df90824c0c1ececc8b42300047f67082
alex <nba4everwanted@gmail.com>
2023-07-13 08:02:05 +0000

    added Browser router in main.jsx, Routes and Route in App.jsx. Added some styles in alignment.css ie w-100 and m-4 and m-5

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,19 @@ import "./stylesheets/form-elements.css"
 import "./stylesheets/custome-components.css"
 import "./stylesheets/alignments.css"
 import "./stylesheets/theme.css"
+import {Routes, Route} from "react-router-dom"
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
 
 function App() {
  
 
   return (
     <div>
-      Wallet App
+      <Routes>
+         <Route path="/login" element={<Login />}/>
+         <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
