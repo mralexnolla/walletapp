edfe78e3e76c41556afc75dbb008444f8918844b
alex <nba4everwanted@gmail.com>
2023-07-13 07:01:37 +0000

    worked on the routing and added some styles in the in the alignment and the also added google fonts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
-import "./stylesheets/text-elements.css"
-import "./stylesheets/form-elements.css"
-import "./stylesheets/custome-components.css"
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
 import "./stylesheets/alignments.css"
+import "./stylesheets/custome-components.css"
+import "./stylesheets/form-elements.css"
+import "./stylesheets/text-elements.css"
 import "./stylesheets/theme.css"
+import {Routes, Route} from "react-router-dom"
 
 function App() {
  
 
   return (
     <div>
-      Wallet App
+     <Routes>
+       <Route path="/login" element={<Login />}/>
+       <Route path="/register" element={<Register />}/>
+     </Routes>
+      
     </div>
   );
 }
